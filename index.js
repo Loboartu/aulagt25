be7979d03a20bc7d5bfac98e9cbc9ad251f6bbcc
Artur <arturllobo@gmail.com>
2025-04-23 13:52:50 -0300

    aula de javaScript

---
+++ index.js
@@ -0,0 +1,34 @@
+alert ("Hello world")
+
+
+// Declarar uma variável com o seu nome 
+// Declarar uma variável com a sua idade
+// Declarar uma variável com a sua comida favorita e não 
+// atribuir valor
+// Atribuir valor a sua comida favorita
+
+let nome = "Artur";
+
+
+let comidaFavorita 
+console.log(comidaFavorita)
+
+comidaFavorita = "pudim";
+console.log (comidaFavorita)
+
+nome= prompt("digite seu nome");
+console.log(nome)
+
+//String
+
+nome = "Artur"
+idade = "28"
+
+//Number
+
+idade = 28;
+let altura = 1.73
+
+//Boolean (true ou false)
+
+const desenvolvedor = true; 
---
+++ tiposdedados.js
@@ -0,0 +1,23 @@
+//tipos de dados
+
+//string
+const escola = "Digital College"
+const curso= ''
+
+//Boolean ou Boleano (true ou false)
+
+let x = 2
+let y = 4
+
+console.log(x > y) //operações de comparação output - > false
+
+//Number
+idade = 28
+
+console.log(typeof(NaN)) //Not a Number
+
+//Undefined
+
+let nomeCompleto;
+
+console.log(nomeCompleto)
